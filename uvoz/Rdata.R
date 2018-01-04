1cb60d536b061e6cb98a1410b7394157ef30d6b5
MatijaGH <31621502+MatijaGH@users.noreply.github.com>
2018-01-04 01:22:17 +0100

    Add files via upload

---
+++ uvoz/Rdata.R
@@ -0,0 +1,37 @@
+# 2. faza: Uvoz podatkov
+
+# Uvozim podatke o uvozu in izvozu iz excela
+install.package('readxl')
+library('readxl')
+gdata
+uvoz <- read_excel(file = 'importFuel.xls')
+
+
+izvoz <- read_excel(file = 'exportFuel.xls')
+
+
+
+
+
+
+#Uvozim podatke o BDPju
+BDP <- read.csv(file = 'BDP2podatki.csv',skip = 4,header = TRUE,
+                na = c('','NY.GDP.MKTP.CD'),
+                colClasses = c(NA,'NULL','NULL','NULL'))
+
+
+#Uvozim podatke o gibanju cen nafte
+
+xmlfile <- xmlParse('http://www.opec.org/basket/basketDayArchives.xml')
+xmltop <- xmlRoot(xmlfile)
+Cene <- ldply(xmlToList('http://www.opec.org/basket/basketDayArchives.xml'),
+              data.frame)
+Cene <- Cene[,2]
+
+
+
+#Uvozim podatke o gibanju vrednosti valut v primerjavi z valuto SDR
+
+valute <- read.csv(file = 'PodatkiOValutah.csv', 
+              header = TRUE,
+              na = c('',' ','-'))
