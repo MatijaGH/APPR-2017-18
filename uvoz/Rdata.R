a401b86fb0a7175590df5a9b60c8a7a4a10a2624
Matija Gubanec Hančič <nano.robot.electric@gmail.com>
2018-02-12 21:45:23 +0100

    Napisal funkcijo, s katero dobim tabele v TidyData obliki iz Excela.

--- uvoz/Rdata.R
+++ uvoz/Rdata.R
@@ -2,11 +2,24 @@
 
 # Uvozim podatke o uvozu in izvozu iz excela
 uvozi_excel <- function(mapa) {
-  read_excel(mapa)
+  uvozeno <- read_excel(mapa,col_names = FALSE)
+  visina <- dim(uvozeno)[1]
+  sirina <- dim(uvozeno)[2]
+  tabela <- array(,c(sirina-1,3))
+  for(vrstica in 1:visina){
+    for(stolpec in 2:sirina){
+      tabela[stolpec-1,1] <- unlist(uvozeno[vrstica,1])
+      tabela[stolpec-1,2] <- as.numeric(uvozeno[1,stolpec])
+      tabela[stolpec-1,3] <- as.numeric(uvozeno[vrstica,stolpec])
+    }
+  }
+  return(tabela)
 }
   
-
 uvoz <- uvozi_excel('podatki/importFuel.xls')
+
+
+
 
 izvoz <- uvozi_excel('podatki/exportFuel.xls')
 
@@ -25,18 +38,18 @@ uvozi_BDP <- function(mapa) {
 BDP <- uvozi_BDP('podatki/BDP2podatki.csv')
 
 #Uvozim podatke o gibanju cen nafte
-
 xmlfile <- xmlParse('http://www.opec.org/basket/basketDayArchives.xml')
 xmltop <- xmlRoot(xmlfile)
 Cene <- ldply(xmlToList('http://www.opec.org/basket/basketDayArchives.xml'),
               data.frame)
 Cene <- Cene[,2]
 
+xmlfile2 <- xmlTreeParse('http://www.opec.org/basket/basketDayArchives.xml')
+topxml <- xmlRoot(xmlfile2)
+topxml <- xmlSApply(topxml,xmlValue)
+xml_df <- data.frame(t(topxml),row.names = NULL)
+
 
 
 #Uvozim podatke o gibanju vrednosti valut v primerjavi z valuto SDR
-uvozi_valute <- function(mapa) {
-  valute <- read.csv(file = mapa, header = TRUE, na = c('',' ','-'))
-}
-
 valute <- uvozi_excel('podatki/PodatkiOValutah.xlsx')
